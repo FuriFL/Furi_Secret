a736a0d15911c54f3f9878efc184c326007e6be3
FuriFL <anothernovem@gmail.com>
2026-02-06 00:18:44 +0700

    main.py

--- main.py
+++ main.py
@@ -555,7 +555,7 @@ async def on_message(message):
             t = data.get("tier", data.get("SPECIAL"))
             if t == tier:
                 value = data.get("value", 0)
-                lines.append(f"• {data.get('full','')} | value: {value} #key: {data.get('key','')}")
+                lines.append(f"• {data.get('full','')} | value: {value} #key: {key}")
 
         if not lines:
             await message.channel.send(f"⚠️ No specs found in **{tier}** tier")
